d9b32f2379db7ebc8c975c8865d26bd671f3ccfb
iksi <hello@iksi.cc>
2013-06-10 13:14:17 +0200

    changed javascript init

--- responsivevideo.js
+++ responsivevideo.js
@@ -1,8 +1,8 @@
-(function($){
+$(function(){
     $(document).ready(function(){
         $('.video').each(function(){
             var iframe = $(this).find('iframe');
             $(this).css('paddingBottom', (100 * iframe.attr('height') / iframe.attr('width'))+'%');
         });
     });
-})(jQuery);
+});
