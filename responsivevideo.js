b10f850181b00b428628960ca22b8de25c6e7b8f
iksi <hello@iksi.cc>
2013-07-15 13:39:01 +0200

    added DOMContentLoaded

--- responsivevideo.js
+++ responsivevideo.js
@@ -1,3 +1,9 @@
-[].forEach.call(document.querySelectorAll('.video > iframe'), function(iframe) {
-    iframe.parentNode.style.paddingBottom = (100 * iframe.getAttribute('height') / iframe.getAttribute('width'))+'%';
-});
+(function() {
+    'use strict';
+    /* global document */
+    document.addEventListener('DOMContentLoaded', function() {
+        [].forEach.call(document.querySelectorAll('.video > iframe'), function(iframe) {
+            iframe.parentNode.style.paddingBottom = (100 * iframe.getAttribute('height') / iframe.getAttribute('width'))+'%';
+        });
+    });
+})();
